5247c385816ebc276e5da4262ea8d1aafc6bc8c8
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-11-28 17:05:04 +0100

    -show duration of movie on remote control - only send movied loading to target

--- public/remote.js
+++ public/remote.js
@@ -12,6 +12,11 @@ const mainRemote = () => {
     remotePlayStop.disabled = !event.detail.clients.list || !event.detail.clients.list.includes(monitorId)
   })
 
+  // handle monitor selection change
+  clientList.addEventListener("change", event => {
+    monitorId = event.target.value
+  })
+
   // handle remote control actions
   remote.addEventListener("click", event => {
     console.log("remoteControl click", event.target.id)
@@ -19,24 +24,27 @@ const mainRemote = () => {
   })
 
   // receiving info from remotcontrol
+  // TODO
   document.addEventListener("remoteControl", event => {
     console.log("remoteControl event", event)
   })
 
-  // load move to monitor
+  // load movie to monitor
   moviesUl.addEventListener("click", event => {
     let elm = event.target
     let limit = 5;
     while (elm.tagName !== "A" && --limit > 0) {
       elm = elm.parentElement
     }
-    const movieToLoad = elm.getAttribute("href").replace(/^\/[^/]*\//, "")
-    if (selectedMovie !== null) {
-      moviesUl.children[selectedMovie].querySelector("a").style.backgroundColor = ""
+    const movieToLoad = elm && elm.getAttribute("href").replace(/^\/[^/]*\//, "")
+    if (movieToLoad) {
+      if (selectedMovie !== null) {
+        moviesUl.children[selectedMovie].querySelector("a").style.backgroundColor = ""
+      }
+      elm.style.backgroundColor = "rgba(128, 0, 0, .2)"
+      ws.send(JSON.stringify({ remoteControl: { movie: movieToLoad }, targetIds: [ monitorId ] }))
+      selectedMovie = [...moviesUl.children].indexOf(elm.parentElement)
     }
-    elm.style.backgroundColor = "rgba(128, 0, 0, .2)"
-    ws.send(JSON.stringify({ remoteControl: { movie: movieToLoad, targetIds: [ monitorId ] } }))
-    selectedMovie = [...moviesUl.children].indexOf(elm.parentElement)
     event.preventDefault()
   })
 
@@ -45,7 +53,21 @@ const mainRemote = () => {
   }).then(response => {
     const dirsUl = document.querySelector("#movies-list .dirs")
     response.data.movies.forEach(movie => {
-      moviesUl.insertAdjacentHTML("beforeend", templateVideoBox.replace(/{{name}}/g, movie.name).replace(/{{url}}/g, movie.url))
+      moviesUl.insertAdjacentHTML("beforeend", templateVideoBox
+        .replace(/{{name}}/g, movie.name.replace(/\..*$/, ""))
+        .replace(/{{url}}/g, movie.url))
+
+      // show duration of video
+      const mov = moviesUl.querySelector("li:last-child video")
+      const loadDuration = (event) => {
+        let duration = new Date(1000*event.target.duration).toISOString().substr(11, 8).replace(/00:/g, "")
+        const num = (duration.match(/:/g) || []).length
+        duration += num == 2 ? "hours" : (num == 1 ? "min": "sec")
+        event.target.parentElement.querySelector(".duration").textContent = duration
+
+        mov.removeEventListener("loadedmetadata", loadDuration)
+      }
+      mov.addEventListener("loadedmetadata", loadDuration)
     })
     response.data.dirs.forEach(dir => {
       dirsUl.insertAdjacentHTML("beforeend", templateUrlBox.replace(/{{name}}/g, dir.name).replace(/{{url}}/g, dir.url))
