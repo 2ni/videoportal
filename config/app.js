591df4879a3ad7daa6b3c30e2e043e207ff4ac0f
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-12-03 10:43:15 +0100

    add production config

--- config/app.js
+++ config/app.js
@@ -1,7 +1,17 @@
 import localhost from "./environment/localhost.js"
+import production from "./environment/production.js"
 
-const env = process.env.ENV || "prod"
+const env = process.env.ENV || "production"
 
-let config = localhost;
+let config;
+switch (env) {
+  case "production":
+    config = production
+    break
+  default:
+  case "localhost":
+    config = localhost
+    break
+}
 
 export { env, config }
