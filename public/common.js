ac3e0f904ebd9a5a765dbfb865e2265734425071
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2022-09-14 08:22:22 +0200

    load sequentially

--- public/common.js
+++ public/common.js
@@ -4,14 +4,16 @@ let trial = 0
 // https://stackoverflow.com/questions/49916259/show-element-when-in-viewport-on-scroll
 const loadVideos = () => {
   const innerHeight = window.innerHeight || document.documentElement.clientHeight
-  document.querySelectorAll("video").forEach(el => {
-    const bb = el.getBoundingClientRect()
+  const videos = document.querySelectorAll("video")
+  // load sequentially
+  for (const video of videos) {
+    const bb = video.getBoundingClientRect()
     // if within viewport and not yet loaded
-    if (el.preload !== "metadata" && !(bb.top > innerHeight || bb.bottom < 0)) {
-      console.log(el.querySelector("source").src, !(bb.top > innerHeight || bb.bottom < 0))
-      el.preload = "metadata"
+    if (video.preload !== "metadata" && !(bb.top > innerHeight || bb.bottom < 0)) {
+      // console.log(video.querySelector("source").src, !(bb.top > innerHeight || bb.bottom < 0))
+      video.preload = "metadata"
     }
-  })
+  }
 }
 
 document.addEventListener("DOMContentLoaded", event => {
